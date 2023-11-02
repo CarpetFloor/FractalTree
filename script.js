5a3d109e6f55aba26f9a5d8e6eed7a7464bf9c11
CarpetFloor <dylan.bartness@gmail.com>
2023-11-02 00:32:25 -0500

    'progress'

--- script.js
+++ script.js
@@ -4,3 +4,30 @@ c.width = window.innerWidth - 17;
 c.height = window.innerHeight - 20;
 
 r.fillStyle = "violet";
+
+let iterations = 10;
+let length = 100;
+// in deg
+let angle = 45;
+let x = c.width / 2;
+let y = (c.height / 2) + 50;
+r.moveTo(x, y);
+r.lineTo(x, y - length);
+r.strokeStyle = "violet";
+r.lineWidth = 4;
+r.stroke();
+r.moveTo(x, y - length);
+// trying to get right side to work
+for(let i = 0; i < iterations; i++) {
+    length *= 0.5;
+
+    angle += 40;
+    let xdiff = length * Math.sin(angle);
+    let ydiff = length * Math.cos(angle);
+
+    x += xdiff;
+    y += ydiff;
+    r.lineTo(x, y);
+    r.stroke();
+    r.moveTo(x, y);
+}
